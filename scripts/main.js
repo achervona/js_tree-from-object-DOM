83b55ad39713e6b0dac2731f4ea82d9b19334f6c
Anastasia Chervona <av.chervona@gmail.com>
2020-10-07 22:59:57 +0300

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const food = {
+  'Meat': {
+    'Pork': {},
+    'Beef': {},
+  },
+
+  'Fruit': {
+    'Red': {
+      'Cherry': {},
+      'Strawberry': {},
+    },
+    'Yellow': {
+      'Banana': {},
+      'Pineapple': {},
+    },
+
+  },
+};
+
+const tree = document.querySelector('#tree');
+
+function createTree(element, data) {
+  const list = document.createElement('ul');
+
+  for (const key in data) {
+    const listItem = document.createElement('li');
+
+    listItem.textContent = key;
+    list.append(listItem);
+
+    if (Object.keys(data[key]).length !== 0) {
+      createTree(listItem, data[key]);
+    }
+  }
+
+  element.append(list);
+}
+
+createTree(tree, food);
